cbaa8d354a1c347fdf856fd5f5a03013f5471390
starrySky <2840777097@qq.com>
2024-11-15 18:23:51 +0800

    项目初始化

---
+++ src/navigation/index.ts
@@ -0,0 +1,21 @@
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CollectScreen from "@/screens/collect";
+import LeaderBoardScreen from "@/screens/leaderBoard";
+import SearchScreen from "@/screens/search";
+import SettingScreen from "@/screens/setting";
+import SongListScreen from "@/screens/songList";
+
+export const RootStack = createNativeStackNavigator({
+  initialRouteName: "SongList",
+  screens: {
+    Collect: CollectScreen,
+    LeaderBoard: LeaderBoardScreen,
+    Search: SearchScreen,
+    Setting: SettingScreen,
+    SongList: SongListScreen,
+  },
+});
+
+export const Navigation = createStaticNavigation(RootStack);
---
+++ src/screens/collect/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Text } from "react-native";
+
+export default function CollectScreen() {
+  return <Text>CollectScreen</Text>;
+}
